f06ff24b027e3ace3b2d7f04c8ca6b578898545e
Manny-27 <mannyalberto2763@gmail.com>
2024-02-06 15:33:23 -0400

    agregando tailwind y configurando el proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 
-  integrations: [
-    tailwind({
-      // Example: Provide a custom path to a Tailwind config file
-      configFile: './custom-config.mjs',
-    }),
-  ]
-
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({}),
+  ]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./src/**/*/*.{astro,html,js,jsx,ms}'],
+    theme: {
+        extend: {
+
+        }
+    },
+    plugins: []
+}
